96458acadfa36fc0e278d2e5269af8c0685be41b
Prashant Kumar <prashantkumar010704@gmail.com>
2023-04-02 11:43:06 +0530

    again2

--- index.js
+++ index.js
@@ -35,19 +35,19 @@ app.get("/", async(req, res) => {
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
-  // // Create client instance for auth
-  // const client = await auth.getClient();
+  // Create client instance for auth
+  const client = await auth.getClient();
 
-  // // Instance of Google Sheets API
-  // const googleSheets = google.sheets({ version: "v4", auth: client });
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
-  // const spreadsheetId = process.env.SPREADSHEETID;
+  const spreadsheetId = process.env.SPREADSHEETID;
 
-  // // Get metadata about spreadsheet
-  // const metaData = await googleSheets.spreadsheets.get({
-  //   auth,
-  //   spreadsheetId,
-  // });
+  // Get metadata about spreadsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
 
   // // Read rows from spreadsheet
   // const getRows = await googleSheets.spreadsheets.values.get({
